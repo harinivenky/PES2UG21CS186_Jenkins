5a2d18d4a81a36a207bdde0fbcd4efa1dcc07257
harinivenky <harini100.venky@gmail.com>
2024-03-06 21:14:14 +0530

    New folder added

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES2UG21CS186 has successfully built and run\n";
+  return 0;
+}
